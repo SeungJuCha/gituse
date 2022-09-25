7669c357e0f8a723dc75c465d5362cbbc418f215
SeungJU Cha <chaseungju990201@gmail.com>
2022-09-25 13:57:12 +0900

    first commit

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 # git init (레포지토리 초기화)
 # 파일 생성 및 작성 등등
-# github url 갸져오기 git remote origin add (URL)
+# github url 갸져오기 git remote add origiin (URL)
+# 혹시 오류가 생길 경우 폴더와 .git 폴더 삭제후 다시 생성해서 해보기
 # git remote -v
 # stage commit  !commit message! 작성
 #  push : git push origin master
